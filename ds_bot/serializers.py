5250213d2424a7fb94f8bef16f56f83cce972187
OmniChain <omnichain@icloud.com>
2025-06-07 13:20:00 +0200

    Update serializers.py

--- ds_bot/serializers.py
+++ ds_bot/serializers.py
@@ -1,1 +1,8 @@
+# ds_bot/serializers.py
+from rest_framework import serializers
+from .models import Item
 
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = '__all__'
