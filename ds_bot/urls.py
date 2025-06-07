a12c4c4dba890c06b72cbff642e6d8047ee3595d
OmniChain <omnichain@icloud.com>
2025-06-07 13:25:28 +0200

    Update urls.py

--- ds_bot/urls.py
+++ ds_bot/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.dashboard, name='dashboard'),
     path('items/', views.item_list, name='item_list'),
     path('items/<int:pk>/', views.item_detail, name='item_detail'),
 ]
