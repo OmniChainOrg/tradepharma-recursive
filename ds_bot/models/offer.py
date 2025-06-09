9cd36b970f1a6c8aedb312325820c08b10c6e845
OmniChain <omnichain@icloud.com>
2025-06-10 00:56:11 +0200

    Update offer.py

--- ds_bot/models/offer.py
+++ ds_bot/models/offer.py
@@ -7,6 +7,3 @@ class Offer(models.Model):
     quantity = models.PositiveIntegerField()
     location = models.CharField(max_length=255)
     status = models.CharField(default='open', max_length=50)
-
-    def __str__(self):
-        return f"{self.item.name} - {self.quantity}"
