2abcd3e0e98d9a762830776a92c649c67c7b013c
OmniChain <omnichain@icloud.com>
2025-06-09 21:45:19 +0200

    Update product.py

--- ds_bot/models/product.py
+++ ds_bot/models/product.py
@@ -1,1 +1,9 @@
+# ds_bot/models/product.py
+from django.db import models
 
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
