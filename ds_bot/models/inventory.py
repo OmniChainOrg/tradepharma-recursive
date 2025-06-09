a38f395f257ee3fc8db19df6f61c1191704f5371
OmniChain <omnichain@icloud.com>
2025-06-10 01:33:46 +0200

    Update inventory.py

--- ds_bot/models/inventory.py
+++ ds_bot/models/inventory.py
@@ -4,7 +4,7 @@ from .item import Item
 
 class Inventory(models.Model):
     name = models.CharField(max_length=255)
-    items = models.ManyToManyField(Item, through='InventoryItem')
+    item = models.ManyToManyField(Item, through='InventoryItem')
 
 class InventoryItem(models.Model):
     inventory = models.ForeignKey(Inventory, on_delete=models.CASCADE)
