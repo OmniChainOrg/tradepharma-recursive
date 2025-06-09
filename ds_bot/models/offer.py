008044903a930c5bda5cf4cf162119351e1c1b32
OmniChain <omnichain@icloud.com>
2025-06-09 23:27:51 +0200

    Update offer.py

--- ds_bot/models/offer.py
+++ ds_bot/models/offer.py
@@ -1,12 +1,12 @@
 # ds_bot/models/offer.py
 from django.db import models
-from .product import Product
+from .item import Item
 
 class Offer(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
     location = models.CharField(max_length=255)
     status = models.CharField(default='open', max_length=50)
 
     def __str__(self):
-        return f"{self.product.name} - {self.quantity}"
+        return f"{self.item.name} - {self.quantity}"
