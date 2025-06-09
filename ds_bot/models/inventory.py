a7b9f1b37620dc5c49cc91be13316a9360bd353e
OmniChain <omnichain@icloud.com>
2025-06-10 01:02:44 +0200

    Update inventory.py

--- ds_bot/models/inventory.py
+++ ds_bot/models/inventory.py
@@ -9,3 +9,6 @@ class Inventory(models.Model):
 class InventoryItem(models.Model):
     inventory = models.ForeignKey(Inventory, on_delete=models.CASCADE)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.inventory} - {self.item}"
