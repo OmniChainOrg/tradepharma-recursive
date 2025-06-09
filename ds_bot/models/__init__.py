dc7850ead53bb25ba2299ec5f45c85be24b77a21
OmniChain <omnichain@icloud.com>
2025-06-09 23:41:58 +0200

    Update __init__.py

--- ds_bot/models/__init__.py
+++ ds_bot/models/__init__.py
@@ -1,7 +1,9 @@
 # ds_bot/models/__init__.py
+from .models import Inventory
+from .models import InventoryItem
 from .item import Item
 from .offer import Offer
 from .demand import Demand
 from .match import Match
 
-__all__ = ['Item', 'Offer', 'Demand', 'Match']
+__all__ = ['Item', 'Offer', 'Demand', 'Match', 'Inventory']
