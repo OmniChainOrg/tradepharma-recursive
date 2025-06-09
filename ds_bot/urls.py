f3aeb5823793c2e29dc4e4d1fa0f1a180bf1e49f
OmniChain <omnichain@icloud.com>
2025-06-09 20:09:11 +0200

    Update urls.py

--- ds_bot/urls.py
+++ ds_bot/urls.py
@@ -1,8 +1,8 @@
 # ds_bot/urls.py
 from django.urls import path
-from . import views
+from ds_bot import views
 
-app_name = 'ds_bot'  # Optional but good practice
+app_name = 'ds_bot'  # Optional but good practice for namespaced URLs
 
 urlpatterns = [
     path('items/', views.ItemViewSet.as_view({'get': 'list'}), name='item-list'),
