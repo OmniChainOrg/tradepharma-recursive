7e6dcf5541ac38632a5968d09324e663519f3e25
OmniChain <omnichain@icloud.com>
2025-06-10 07:26:00 +0200

    Update admin.py

--- ds_bot/admin.py
+++ ds_bot/admin.py
@@ -1,27 +1,18 @@
+# ds_bot/admin.py
+
 from django.contrib import admin
-from .models import Item
-from .models import Inventory
-from .models import InventoryItem
+from ds_bot.models import Item, Inventory
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name', 'description')
 
 
 @admin.register(Inventory)
 class InventoryAdmin(admin.ModelAdmin):
-    list_display = ('user', 'date_added')
-    list_filter = ('user',)
-    search_fields = ('user__name',)
-
-@admin.register(InventoryItem)
-class InventoryItemAdmin(admin.ModelAdmin):
-    list_display = ('inventory', 'item')
-    list_filter = ('inventory', 'item')
-    search_fields = ('inventory__user__name', 'item__name')
-
-@admin.register(Item)
-class ItemAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'price', 'stock', 'date_added', 'user')
-    list_filter = ('user', 'available')
+    list_display = ('name',)
     search_fields = ('name',)
-
 
 
 
